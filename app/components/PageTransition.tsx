2f0bd3361e324af0a1c7010b17eb35f04495b8ed
Snowly <visionhyronic@gmail.com>
2025-02-25 11:48:12 +0100

    remove motion div so its better

--- app/components/PageTransition.tsx
+++ app/components/PageTransition.tsx
@@ -7,17 +7,9 @@ export default function PageTransition({ children }: { children: React.ReactNode
   const pathname = usePathname()
 
   return (
-    <AnimatePresence mode="wait">
-      <motion.div
-        key={pathname}
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        exit={{ opacity: 0, y: -20 }}
-        transition={{ duration: 0.3, ease: "easeInOut" }}
-        className="h-full flex items-center"
-      >
+    <AnimatePresence mode="wait" initial={false}>
+
         {children}
-      </motion.div>
     </AnimatePresence>
   )
 } 

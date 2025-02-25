e1f73803b1a5e6ad84ca22ba61504adbdb695b61
Snowly <visionhyronic@gmail.com>
2025-02-25 11:49:51 +0100

    fix unused-var issue

--- app/components/PageTransition.tsx
+++ app/components/PageTransition.tsx
@@ -1,10 +1,9 @@
 'use client'
 
-import { motion, AnimatePresence } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import { usePathname } from 'next/navigation'
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
-  const pathname = usePathname()
 
   return (
     <AnimatePresence mode="wait" initial={false}>

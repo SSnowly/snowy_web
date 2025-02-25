bac6c55dc71dc52bc8343300eb72753d510639c4
Snowly <visionhyronic@gmail.com>
2025-02-25 11:50:33 +0100

    dumbass me

--- app/components/PageTransition.tsx
+++ app/components/PageTransition.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { AnimatePresence } from 'framer-motion'
-import { usePathname } from 'next/navigation'
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
 
